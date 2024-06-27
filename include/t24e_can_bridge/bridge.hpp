a22c0dea709fb44e1aae3631f7fdec81d5f07159
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-06-27 23:58:32 +0100

    create the basic functionality

---
+++ include/t24e_can_bridge/bridge.hpp
@@ -0,0 +1,55 @@
+#include "rclcpp/rclcpp.hpp"
+#include "lart_msgs/msg/dynamics_cmd.hpp"
+#include "lart_msgs/msg/dynamics.hpp"
+#include <linux/can.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <stdint.h>
+
+#define T24E_CAN_INTERFACE "can0"
+
+#define VCU_CMD_CAN_ID 0x500
+#define VCU_CMD_RPM_BYTE 0
+#define VCU_CMD_RPM_LEN 2
+
+#define VCU_PWT_CAN_ID 0x510
+#define VCU_PWT_RPM_BYTE 0
+#define VCU_PWT_RPM_LEN 2
+
+
+#define DYNAMICS_CMD_TOPIC "pc_origin/dynamics"
+#define DYNAMICS_TOPIC "acu_origin/dynamics"
+
+class Bridge : public rclcpp::Node
+{
+	public:
+		Bridge();
+
+    private:
+        
+        /*! \brief CAN socket descriptor. */
+        int s = -1;
+
+        /*! \brief Subscriber from the control node. */
+        rclcpp::Subscription<lart_msgs::msg::DynamicsCMD>::SharedPtr cmd_subscriber;
+
+        /*! \brief Publisher to the control node with wheel-related information and RPM. */
+        rclcpp::Publisher<lart_msgs::msg::Dynamics>::SharedPtr dynamics_publisher;
+
+        /*! \brief Last command message. */
+        lart_msgs::msg::DynamicsCMD last_cmd;
+
+        /*! \brief Last dynamics message. */
+        lart_msgs::msg::Dynamics last_dynamics;
+
+        /*! \brief Handle a CAN frame data. */
+        void handle_can_frame(struct can_frame frame);
+
+        /*! \brief Send a CAN frame. */
+        void send_can_frame(struct can_frame frame);
+
+        /*! \brief Convert an array of bytes to an unsigned integer. */
+        static uint32_t int_from_bytes(uint8_t *bytes, size_t len);
+};
--- src/bridge.cpp
+++ src/bridge.cpp
@@ -1,11 +1,77 @@
-#include <cstdio>
-#include <linux/can.h>
+#include "t24e_can_bridge/bridge.hpp"
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+Bridge::Bridge() : Node("t24e_can_bridge") {
 
-  printf("hello world t24e_can_bridge package\n");
-  return 0;
+	// create a socket
+	if((this->s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		RCLCPP_ERROR(this->get_logger(), "Failed to create socket");
+		return;
+	}
+
+	// define the interface
+	struct ifreq ifr;
+	strcpy(ifr.ifr_name, T24E_CAN_INTERFACE);
+	ioctl(this->s, SIOCGIFINDEX, &ifr);
+
+	// bind the socket to the CAN interface
+	struct sockaddr_can addr;
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+	if(bind(this->s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		RCLCPP_ERROR(this->get_logger(), "Failed to bind socket");
+		return;
+	}
+
+	// initialize publisher and subscriber
+	this->cmd_subscriber = this->create_subscription<lart_msgs::msg::DynamicsCMD>(
+		DYNAMICS_CMD_TOPIC, 10, [this](lart_msgs::msg::DynamicsCMD::UniquePtr msg) {
+			// TODO
+		});
+
+	this->dynamics_publisher = this->create_publisher<lart_msgs::msg::Dynamics>(DYNAMICS_TOPIC, 10);
+
+	// read CAN frames
+	struct can_frame frame;
+	while (rclcpp::ok()) {
+		int nbytes = read(s, &frame, sizeof(frame));
+		if(nbytes < 0) {
+			RCLCPP_ERROR(this->get_logger(), "Failed to read CAN frame");
+			return;
+		}
+
+		handle_can_frame(frame);
+	}
+}
+
+void Bridge::send_can_frame(struct can_frame frame) {
+	if(write(this->s, &frame, sizeof(frame)) < 0) {
+		RCLCPP_ERROR(this->get_logger(), "Failed to send CAN frame");
+	}
+}
+
+void Bridge::handle_can_frame(struct can_frame frame) {
+	if(frame.can_id == VCU_PWT_CAN_ID) {
+		RCLCPP_INFO(this->get_logger(), "Received VCU_PWT_CAN_ID frame");
+		uint32_t data = int_from_bytes(frame.data, VCU_PWT_RPM_LEN);
+
+		lart_msgs::msg::Dynamics dynamics;
+
+		dynamics.rpm = data;
+	}
+}
+
+uint32_t Bridge::int_from_bytes(uint8_t *bytes, size_t len) {
+	uint32_t result = 0;
+	for(size_t i = 0; i < len; i++) {
+		result |= bytes[i] << (8 * i);
+	}
+	return result;
+}
+
+int main(int argc, char ** argv) {
+
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<Bridge>());
+	rclcpp::shutdown();
+	return 0;
 }
